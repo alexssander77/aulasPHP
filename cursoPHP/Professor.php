53de3004b1c44f9df0fe5401dc4763a95e527698
Alexssander José de Oliveira de Castro <147998833+alexssander77@users.noreply.github.com>
2025-03-19 21:29:24 -0300

    Add files via upload

--- cursoPHP/Professor.php
+++ cursoPHP/Professor.php
@@ -3,13 +3,15 @@
 
     class Professor extends Funcionario{//subclass
         var $disciplina;
-        public function_construct($n, $s, $d){
-            parent::_construct($n, $s);
+        public function __construct($n, $s, $d){
+            parent::__construct($n, $s);
             $this->disciplina = $d;
         }
+
         public function setDisciplina($disciplina){
             $this->disciplina = $disciplina;
         }
+
         static function teste(){
             return "Método de classe";
         }
--- cursoPHP/cadastro.php
+++ cursoPHP/cadastro.php
@@ -2,9 +2,8 @@
     $nome = $_GET["nome"];
     $tutor = $_GET["tutor"];
     echo "Nome: $nome e Tutor: $tutor <br/><br/>";
-    $con = mysqli_connect("localhost", "root", "vertrigo", "veterinaria");
-    $sql = "INSERT INTO animal (nome, tutor) values ('$nome', '$tutor')";
+    $con = mysqli_connect("localhost","root","vertrigo","veterinaria");
+    $sql = "INSERT INTO animal (nome,tutor) VALUES('$nome' , '$tutor')";
 
     $resultado = mysqli_query($con, $sql);
-
 ?>
